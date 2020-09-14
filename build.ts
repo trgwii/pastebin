aa2f64d1cf8dd031d87b01ad36aa087b90ebc019
Thomas Rory Gummerson <thomas@gummerson.no>
2020-09-15 00:36:17 +0200

    Build script

---
+++ build.ts
@@ -0,0 +1,51 @@
+// deno run -A build.ts
+
+const noop = () => {};
+
+const rm = (path: string) => Deno.remove(path, { recursive: true }).catch(noop);
+
+const monacoCleanup = () =>
+  Promise.all([
+    rm("node_modules"),
+    rm("package-lock.json"),
+    rm("monaco-editor.bin"),
+  ]);
+
+const exec = (cmd: string[]) =>
+  Deno.run({
+    cmd: [
+      ...Deno.build.os === "windows" ? ["cmd.exe", "/c"] : [],
+      ...cmd,
+    ],
+  }).status();
+
+const monaco = async () => {
+  await monacoCleanup();
+  await exec(["npm", "install", "--production", "monaco-editor"]);
+  await exec([
+    "bundler",
+    "compress",
+    "node_modules/monaco-editor/min/vs",
+    "monaco-editor.bin",
+  ]);
+  await rm("monaco-editor.bin.ts");
+  await exec([
+    "bundler",
+    "ts-bundle",
+    "monaco-editor.bin",
+    "monaco-editor.bin.ts",
+  ]);
+  await monacoCleanup();
+};
+
+const pub = async () => {
+  await rm("public.bin");
+  await exec(["bundler", "compress", "public", "public.bin"]);
+  await rm("public.bin.ts");
+  await exec(["bundler", "ts-bundle", "public.bin", "public.bin.ts"]);
+  await rm("public.bin");
+};
+
+await monaco();
+await pub();
+await exec(["deno", "fmt"]);
