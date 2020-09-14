90a9a93a3d5ff6143faddc33ba186144fc2e4333
Thomas Rory Gummerson <thomas@gummerson.no>
2020-09-14 23:50:21 +0200

    Remove grab.ts

--- grab.ts
+++
@@ -1,6 +0,0 @@
-export const grab = (path: string, cwd = import.meta.url) => {
-  const url = new URL(path, cwd);
-  return url.protocol === "file:" ? Deno.readFile(url) : (fetch(url)
-    .then((res) => res.arrayBuffer())
-    .then((data) => new Uint8Array(data)));
-};
