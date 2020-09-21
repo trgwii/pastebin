6af9d3c6495711a1225af4753d879bad32f24ba6
Thomas Rory Gummerson <thomas@gummerson.no>
2020-09-21 13:23:22 +0200

    Update std, monaco-editor 0.21.0

--- public.bin.ts
+++ public.bin.ts
@@ -1,5 +1,5 @@
 import { parse } from "https://git.rory.no/trgwii/Bundler/raw/branch/master/parse.ts";
-import { decode } from "https://deno.land/std@0.69.0/encoding/ascii85.ts";
+import { decode } from "https://deno.land/std@0.70.0/encoding/ascii85.ts";
 import { decompress } from "https://deno.land/x/brotli@v0.1.4/mod.ts";
 export default parse(
   decompress(
