476d5048a4424ee110ca292348fad5b9e2f4e736
Thomas Rory Gummerson <thomas@gummerson.no>
2020-09-21 13:16:20 +0200

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-export { encode } from "https://deno.land/std@0.69.0/encoding/base64url.ts";
-export { createHash } from "https://deno.land/std@0.69.0/hash/mod.ts";
+export { encode } from "https://deno.land/std@0.70.0/encoding/base64url.ts";
+export { createHash } from "https://deno.land/std@0.70.0/hash/mod.ts";
 export {
   Response,
   serve,
   Server,
   ServerRequest,
-} from "https://deno.land/std@0.69.0/http/server.ts";
-export { v4 } from "https://deno.land/std@0.69.0/uuid/mod.ts";
+} from "https://deno.land/std@0.70.0/http/server.ts";
+export { v4 } from "https://deno.land/std@0.70.0/uuid/mod.ts";
 export type { bundle } from "https://git.rory.no/trgwii/Bundler/raw/branch/master/types.ts";
