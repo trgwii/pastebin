affa8678cf547688539440580ef8e32ccd2ca639
Thomas Rory Gummerson <thomas@gummerson.no>
2020-10-12 12:36:54 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-useradd -rm pastebin
+useradd -s /bin/bash -rm pastebin
 su pastebin -c "curl -fsSL https://deno.land/x/install/install.sh | sh"
 su pastebin -c "echo 'export DENO_INSTALL=\"/home/pastebin/.deno\"' >> ~/.bash_profile"
 su pastebin -c "echo 'export PATH=\"\$DENO_INSTALL/bin:\$PATH\"' >> ~/.bash_profile"
-su pastebin -c "deno cache --reload https://git.rory.no/trgwii/pastebin/raw/branch/master/serve.ts"
+su pastebin -c ". ~/.bash_profile; deno cache --reload https://git.rory.no/trgwii/pastebin/raw/branch/master/serve.ts"
 cat << EOF > /etc/systemd/system/pastebin.service
 [Unit]
 Description=Pastebin Server
