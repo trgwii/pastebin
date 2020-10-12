6e15f65c6f417bdab468a6682462ad03137f02e3
Thomas Rory Gummerson <thomas@gummerson.no>
2020-10-12 13:00:00 +0200

    Update path to binary

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ After=network.target
 Type=simple
 User=pastebin
 WorkingDirectory=/home/pastebin
-ExecStart=deno run --allow-net --allow-read=pastes --allow-write=pastes https://git.rory.no/trgwii/pastebin/raw/branch/master/serve.ts 8080 127.0.0.1
+ExecStart=/home/pastebin/.deno/bin/deno run --allow-net --allow-read=pastes --allow-write=pastes https://git.rory.no/trgwii/pastebin/raw/branch/master/serve.ts 8080 127.0.0.1
 Restart=on-failure
 
 [Install]
