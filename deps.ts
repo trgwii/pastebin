ba8d9fb7a1ca66965667d978f9f667318a2b5888
Thomas Rory Gummerson <thomas@gummerson.no>
2020-11-16 12:25:36 +0100

    Update deps, use GitHub

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
-export { encode } from "https://deno.land/std@0.76.0/encoding/base64url.ts";
-export { createHash } from "https://deno.land/std@0.76.0/hash/mod.ts";
-export { serve } from "https://deno.land/std@0.76.0/http/server.ts";
+export { encode } from "https://deno.land/std@0.78.0/encoding/base64url.ts";
+export { createHash } from "https://deno.land/std@0.78.0/hash/mod.ts";
+export { serve } from "https://deno.land/std@0.78.0/http/server.ts";
 export type {
   Response,
   Server,
   ServerRequest,
-} from "https://deno.land/std@0.76.0/http/server.ts";
-export { v4 } from "https://deno.land/std@0.76.0/uuid/mod.ts";
+} from "https://deno.land/std@0.78.0/http/server.ts";
+export { v4 } from "https://deno.land/std@0.78.0/uuid/mod.ts";
 export type {
   bundle,
-} from "https://git.rory.no/trgwii/Bundler/raw/branch/master/types.ts";
+} from "https://raw.githubusercontent.com/trgwii/bundler/master/types.ts";
--- deps_test.ts
+++ deps_test.ts
@@ -1,5 +1,5 @@
-export type { ServerRequest } from "https://deno.land/std@0.76.0/http/server.ts";
+export type { ServerRequest } from "https://deno.land/std@0.78.0/http/server.ts";
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.76.0/testing/asserts.ts";
+} from "https://deno.land/std@0.78.0/testing/asserts.ts";
