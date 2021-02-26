83ac5f466e20dfbac228f06bd15375d4590a09d0
Thomas Rory Gummerson <thomas@gummerson.no>
2021-02-26 12:45:53 +0100

    Update

--- assets.b.ts
+++ assets.b.ts
@@ -1,4 +1,4 @@
-import { decode } from "https://deno.land/std@0.83.0/encoding/ascii85.ts";
+import { decode } from "https://deno.land/std@0.88.0/encoding/ascii85.ts";
 import { decompress } from "https://deno.land/x/brotli@v0.1.4/mod.ts";
 import { parse } from "https://raw.githubusercontent.com/trgwii/bundler/master/parse.ts";
 export default parse(
