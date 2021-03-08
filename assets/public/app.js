25d7aefa955f0ff40777a010969a806586b593d4
Thomas Rory Gummerson <thomas@gummerson.no>
2021-03-08 16:12:46 +0100

    Frontend fixes

--- assets/public/app.js
+++ assets/public/app.js
@@ -1,5 +1,6 @@
 ///<reference path="./dts/lib.dom.d.ts" />
 ///<reference path="./dts/lib.monaco.d.ts" />
+///<reference path="./dts/lib.deno.d.ts" />
 (function () {
   var document = window.document;
   var HTMLElement = window.HTMLElement;
@@ -7,6 +8,22 @@
   var history = window.history;
   /** @type {(m: string[], cb: () => void) => void} */
   var require = window.require;
+
+  function getText(/** @type {string} */ url) {
+    return fetch(url).then(function (res) {
+      return res.text();
+    });
+  }
+
+  function addLib(
+    /** @type {string} */ name,
+    /** @type {string} */ url,
+    /** @type {monaco.languages.typescript.LanguageServiceDefaults} */ tsDefs,
+  ) {
+    return getText(url).then(function (text) {
+      tsDefs.addExtraLib(text, name);
+    });
+  }
 
   function create(/** @type {string} */ data, /** @type {string} */ language) {
     return fetch("/", {
@@ -44,19 +61,12 @@
     if (!(main instanceof HTMLElement)) {
       return;
     }
+    var tsDefs = monaco.languages.typescript.typescriptDefaults;
     return Promise.all([
-      fetch("/js/dts/lib.dom.d.ts").then(function (res) {
-        return res.text();
-      }),
-      fetch("/js/dts/lib.deno.d.ts").then(function (res) {
-        return res.text();
-      }),
+      addLib("lib.dom.d.ts", "/js/dts/lib.dom.d.ts", tsDefs),
+      addLib("lib.deno.d.ts", "/js/dts/lib.deno.d.ts", tsDefs),
     ])
-      .then(function (libs) {
-        var dom = libs[0];
-        var deno = libs[1];
-        window.monaco.languages.typescript.typescriptDefaults.addExtraLib(dom);
-        window.monaco.languages.typescript.typescriptDefaults.addExtraLib(deno);
+      .then(function () {
         var editor = monaco.editor.create(main, {
           renderWhitespace: "all",
           theme: "vs-dark",
